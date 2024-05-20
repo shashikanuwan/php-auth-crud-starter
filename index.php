749fb273cb13dae94238a5e1a195625211e88695
Nuwan Sampath <sampath@itplace.com>
2024-05-20 18:54:04 +0530

    connect database using PDO

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
 
 require "function.php";
-require "router.php";
+require "Database.php";
+$config = require "config.php";
+// require "router.php";
+
+$db = new Database($config['database']);
+
+$id = $_GET['id'] ?? null;
+$query = "SELECT * FROM posts where id = ?";
+
+$post = $db->query($query, [$id])->fetch();
+
+dd($post);
