70f36efa24b0fd863542e94b3cbcce6d201f8ae0
Nuwan Sampath <sampath@itplace.com>
2024-05-28 19:04:17 +0530

    add authorization

--- controllers/NoteController.php
+++ controllers/NoteController.php
@@ -3,6 +3,16 @@
 $config = require "config.php";
 $db = new Database($config['database']);
 
-$note = $db->query('SELECT * FROM notes WHERE id = :id', ['id' => $_GET['id']])->fetch();
+$note = $db->query('SELECT * FROM notes WHERE id = :id', [
+    'id' => $_GET['id']
+])->fetch();
+
+if (!$note) {
+    abort();
+}
+
+if ($note['user_id'] != 1) {
+    abort(403);
+}
 
 require "views/note.view.php";
