5ab437024ccf8001633b18a6a9d41674f2588dbd
Nuwan Sampath <sampath@itplace.com>
2024-05-29 18:40:36 +0530

    move function.php and router.php to Core

--- Core/router.php
+++ Core/router.php
@@ -1,11 +1,9 @@
 <?php
 
-$routes = require base_path('routes.php');
-
 function routeToController($uri, $routes)
 {
     if (array_key_exists($uri, $routes)) {
-        require $routes[$uri];
+        require base_path($routes[$uri]);
     } else {
         abort();
     }
@@ -20,6 +18,7 @@ function abort($code = 404)
     die();
 }
 
+$routes = require base_path('routes.php');
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 routeToController($uri, $routes);
